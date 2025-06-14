c6d244248276cf3d7b57ede05ce26f07ed202113
Rajat <63333718+Rajat545@users.noreply.github.com>
2025-06-14 13:38:06 +0530

    Update install-k8s.sh

--- k8s/install-k8s.sh
+++ k8s/install-k8s.sh
@@ -3,7 +3,6 @@
 set -e
 
 echo "🔧 Installing Kubernetes CLI tools: kind, kubectl, and helm..."
-
 # -------------------
 # Install K3s cluster && Kubectl
 # -------------------
@@ -15,21 +14,33 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.bashrc
 source ~/.bashrc
 
+#Install kubectl
+
+# Variables
+VERSION="v1.30.0"
+URL="https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl"
+INSTALL_DIR="/usr/local/bin"
+
 # Download and install kubectl
 curl -LO "$URL"
 chmod +x kubectl
 sudo mv kubectl $INSTALL_DIR/
 kubectl version --client
 
+# Clean up
+rm -f kubectl
+
+echo "kubectl installation complete."
 
 echo "k3s cluster & kubectl installation complete."
-
 
 # -------------------
 # Install Helm
 # -------------------
 echo "📦 Installing Helm..."
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 echo "✅ Helm installed: $(helm version --short)"
 
 echo "🎉 All tools installed successfully!"
